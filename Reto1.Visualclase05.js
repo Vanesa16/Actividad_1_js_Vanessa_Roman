dd2e9eaa380ecfa2eea5bb265d064da3766cc3b2
Vanesa16 <vanesalondon11@gmail.com>
2023-08-01 19:48:06 +0100

    clase 5

---
+++ Reto1.Visualclase05.js
@@ -0,0 +1,11 @@
+let name = "Vanessa"
+let last_name= "Roman"
+const birth_year = "2003"
+let city = "Medellin"
+
+console.log("hello my name is", name, last_name, "i was born in" , birth_year, "i live in", city, "and i am 22 years old");
+
+
+
+
+
